0a2e3b4d02598337898fc970626c92570f5d500d
A3Tom <Tom_Esq@Yahoo.com>
2024-02-04 21:39:31 +0000

    Added get by detail into project repo

--- server/app/projects/repository.py
+++ server/app/projects/repository.py
@@ -1,10 +1,14 @@
+from sqlalchemy import select
 from app.base_repository import BaseRepository
 from .models import Project
 from app.database import engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
 
 class ProjectsRepository(BaseRepository):
     def __init__(self):
         super().__init__(Project)
     
-    
+    def get_by_id_detail(self, id):
+        with Session(engine) as session:
+            stmt = select(Project).options(selectinload(Project.client)).filter(Project.id == id)
+            return session.scalars(stmt).first()

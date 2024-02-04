e295051a1b441277f1ed4d225ed8b88a2a1ac7bd
A3Tom <Tom_Esq@Yahoo.com>
2024-02-04 00:25:10 +0000

    Fleshed oot some project routes

--- server/app/routes.py
+++ server/app/routes.py
@@ -64,7 +64,46 @@ def get_projects():
     projects = db.session.query(Project).all()
     return jsonify([ project.to_dict() for project in projects]), 200
 
+@app.route('/projects/<int:project_id>', methods=['GET'])
+def get_project_by_id(project_id):
+    project = db.session.query(Project).get(project_id)
+    if project is None:
+        return {}, 200
+
+    return project.to_dict(), 200
+    # return [ project.id for project in projects], 200
+
+@app.route('/projects', methods=['POST'])
+def create_project():
+    data = request.json
+    try:
+        new_project = Project(**data)
+        db.session.add(new_project)
+        db.session.commit()
+        return jsonify({'message': f'New project created ([{new_project.id}] {new_project.name})'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+# Delete a project
+@app.route('/projects/<int:project_id>', methods=['DELETE'])
+def delete_project(project_id):
+    project = db.session.query(Project).get(project_id)
+    if project is None:
+        return jsonify({'message': f'Project with id {project_id} cannnot be found'}), 200
+    
+    db.session.delete(project)
+    db.session.commit()
+    return jsonify({'message': f'Project deleted ([{project.id}] {project.name})'}), 200
+
 @app.route('/joins', methods=['GET'])
 def get_join():
     projects = db.session.query(Project).join(Project.client).all()
-    return jsonify([ project.to_dict() for project in projects]), 200
+    return [ format_project_output(project) for project in projects], 200
+
+def format_project_output(project):
+    return {
+        'Project Id': project.id, 
+        'Project': project.name, 
+        'Client Id': project.client.id, 
+        'Client': project.client.name
+    }

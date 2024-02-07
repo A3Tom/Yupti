b66fe960cfe2bdae980ce3d779d3f006d4255736
A3Tom <Tom_Esq@Yahoo.com>
2024-02-07 21:57:08 +0000

    Registered routes

--- server/run.py
+++ server/run.py
@@ -2,12 +2,14 @@ from flask import Flask
 from instance.config import Config
 from app.clients.routes import clients_bp
 from app.projects.routes import projects_bp
+from app.time_entries.routes import time_entries_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 app.register_blueprint(clients_bp, url_prefix='/clients')
 app.register_blueprint(projects_bp, url_prefix='/projects')
+app.register_blueprint(time_entries_bp, url_prefix='/timeentries')
 
 if __name__ == '__main__':
     app.run(debug=True)

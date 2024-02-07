df9e62fdf40c3a1040cceb8409a7759a3269b4c6
A3Tom <Tom_Esq@Yahoo.com>
2024-02-07 21:56:01 +0000

    Removed method now in base repo

--- server/app/projects/repository.py
+++ server/app/projects/repository.py
@@ -7,8 +7,3 @@ from sqlalchemy.orm import Session, selectinload
 class ProjectsRepository(BaseRepository):
     def __init__(self):
         super().__init__(Project)
-    
-    def get_by_id_detail(self, id):
-        with Session(engine) as session:
-            stmt = select(Project).options(selectinload(Project.client)).filter(Project.id == id)
-            return session.scalars(stmt).first()

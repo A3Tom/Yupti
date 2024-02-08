b0c6577f8e8e9c1eb63f9d8920a05e701fe35561
A3Tom <Tom_Esq@Yahoo.com>
2024-02-08 21:42:57 +0000

    Built out tags data model and association table correctly

--- server/app/tags/models.py
+++ server/app/tags/models.py
@@ -1,24 +1,25 @@
-from sqlalchemy import ForeignKey
+from typing import List
+from sqlalchemy import Column, ForeignKey, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.database import Base
-from app.time_entries.models import TimeEntry
+
+timeentry_tag_association_table = Table(
+    "TimeEntryTags",
+    Base.metadata,
+    Column("TagID", ForeignKey("Tags.TagID"), primary_key=True),
+    Column("TimeEntryID", ForeignKey("TimeEntries.TimeEntryID"), primary_key=True),
+)
 
 class Tag(Base):
     __tablename__ = 'Tags'
     id: Mapped[int] = mapped_column('TagID', primary_key=True)
     name: Mapped[str] = mapped_column()
+    time_entries: Mapped[List["TimeEntry"]] = relationship(
+        secondary=timeentry_tag_association_table, back_populates="tags"
+    )
 
     def to_dict(self):
         return {
             'id': self.id,
             'name': self.name
         }
-
-# Ahhh I know this isn't how you do this really and it probably won't work - will get working later
-class TimeEntryTag(Base):
-    __tablename__ = 'TimeEntryTags'
-    tag_id: Mapped[int] = mapped_column('TagID', ForeignKey("Tags.TagID"))
-    tag = relationship(Tag, backref="Tag")
-    
-    time_entry_id: Mapped[int] = mapped_column('TimeEntryID', ForeignKey("TimeEntries.TimeEntryID"))
-    time_entry = relationship(TimeEntry, backref="time_entries")

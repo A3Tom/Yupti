9a16724a28efbea0f52e5698e607e5526acd2950
A3Tom <Tom_Esq@Yahoo.com>
2024-02-08 21:43:18 +0000

    registered tags blueprint

--- server/run.py
+++ server/run.py
@@ -3,6 +3,7 @@ from instance.config import Config
 from app.clients.routes import clients_bp
 from app.projects.routes import projects_bp
 from app.time_entries.routes import time_entries_bp
+from app.tags.routes import tags_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +11,7 @@ app.config.from_object(Config)
 app.register_blueprint(clients_bp, url_prefix='/clients')
 app.register_blueprint(projects_bp, url_prefix='/projects')
 app.register_blueprint(time_entries_bp, url_prefix='/timeentries')
+app.register_blueprint(tags_bp, url_prefix='/tags')
 
 if __name__ == '__main__':
     app.run(debug=True)

273ef5c16c25d2a409f8ac9e27758d8c40fbfb1f
A3Tom <Tom_Esq@Yahoo.com>
2024-02-12 23:18:31 +0000

    Added AWS config malarkey

--- server/instance/config.py
+++ server/instance/config.py
@@ -6,3 +6,7 @@ class Config:
     
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'database_uri_not_found')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    
+    AWS_USER_POOL_ID = os.getenv('AWS_USER_POOL_ID', 'aws_user_pool_id_not_found')
+    AWS_APP_CLIENT_ID = os.getenv('AWS_APP_CLIENT_ID', 'aws_app_client_id_not_found')
+    AWS_APP_CLIENT_SECRET = os.getenv('AWS_APP_CLIENT_SECRET', 'aws_app_client_secret_not_found')

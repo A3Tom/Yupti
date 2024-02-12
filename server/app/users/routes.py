aa0098829f4d29ff5cb1a96428a129b47375b077
A3Tom <Tom_Esq@Yahoo.com>
2024-02-12 23:18:46 +0000

    Added some sorta-sample code to try

--- server/app/users/routes.py
+++ server/app/users/routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, request, jsonify, make_response
 from app.common.cognito_identity_wrapper import CognitoIdentityProviderWrapper
+from .models import CreateUserDTO
+import boto3
+from instance.config import Config
 
 users_bp = Blueprint('users', __name__)
 
@@ -11,4 +14,19 @@ def get_users():
 # create a user post route
 @users_bp.route('/', methods=['POST'])
 def create_user():
-    return jsonify({'message': 'Create user route'}), 201
+    data = request.get_json()
+    create_user = CreateUserDTO(**data)
+
+    cog_wrapper = CognitoIdentityProviderWrapper(
+        boto3.client("cognito-idp"), Config.AWS_USER_POOL_ID, Config.AWS_APP_CLIENT_ID, Config.AWS_APP_CLIENT_SECRET
+    )
+
+    confirmed = cog_wrapper.sign_up_user(
+        create_user.username,
+        create_user.password, 
+        create_user.email)
+
+    if not confirmed:
+        return make_response(jsonify({"error": "Error when creating the user"}), 400)
+    
+    return jsonify({'message': f'Fuckin Yaldiiiiiiiii, {create_user.name} ya wee bas - yer in!'}), 201
